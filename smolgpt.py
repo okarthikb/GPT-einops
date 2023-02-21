f456e57073da9fc13196684d8a2427780bc7a9c0
okarthikb <86470305+okarthikb@users.noreply.github.com>
2023-02-20 20:16:59 -0500

    Update smolgpt.py

--- smolgpt.py
+++ smolgpt.py
@@ -6,28 +6,34 @@ from einops import rearrange
 from math import sqrt
 
 
-# GELU(x) = x * ϕ(x) where ϕ is the CDF of the Gaussian distribution
 class GELU(nn.Module):
   def forward(self, x):
     return x * 0.5 * (1 + torch.erf(x / sqrt(2)))
 
 
 class LayerNorm(nn.Module):
-  def __init__(self, d, eps=1e-5):
+  def __init__(self, shape, eps=1e-5, affine=True):
     super().__init__()
-    self.d, self.eps = d, eps
-    self.w = nn.Parameter(torch.tensor(1.))
-    self.b = nn.Parameter(torch.tensor(0.))
+    if isinstance(shape, int):
+      self.shape = torch.Size([shape])
+    elif isinstance(shape, list):
+      self.shape = torch.Size(shape)
+    assert isinstance(self.shape, torch.Size), 'invalid type for shape'
+    self.dims = [-(i + 1) for i in range(len(self.shape))]
+    self.eps, self.affine = eps, affine
+    if affine:
+      self.gamma = nn.Parameter(torch.ones(self.shape))
+      self.beta = nn.Parameter(torch.zeros(self.shape))
 
   def forward(self, x):
-    for i in range(len(x.shape) - 1, -1, -1):
-      if x.shape[i] == self.d:
-        d = i
-        break
-    mu = x.mean(d).unsqueeze(d)  # E[x]
-    mu_ = (x * x).mean(d).unsqueeze(d)  # E[x^2]
-    sigma = torch.sqrt(mu_ - mu * mu + self.eps)
-    return self.w * (x - mu) / sigma + self.b
+    assert x.shape[-len(self.shape):] == self.shape, 'invalid input shape'
+    mu = x.mean(dim=self.dims, keepdim=True)  # E[x]
+    mu2 = (x * x).mean(dim=self.dims, keepdim=True)  # E[x^2]
+    var = mu2 - (mu * mu) + self.eps  # σ^2 = E[x^2] - E[x]^2
+    ln = (x - mu) / torch.sqrt(var)
+    if self.affine:
+      ln = self.gamma * ln + self.beta
+    return ln
 
 
 class Layer(nn.Module):
@@ -37,9 +43,7 @@ class Layer(nn.Module):
     self.d, self.nh, self.h = d, nh, d // nh
     self.wx, self.wo = nn.Linear(d, 3 * d), nn.Linear(d, d)
     self.ln1, self.ln2 = LayerNorm(d), LayerNorm(d)
-    self.ffn = nn.Sequential(
-      nn.Linear(d, 4 * d), GELU(), nn.Linear(4 * d, d)
-    )
+    self.ffn = nn.Sequential(nn.Linear(d, 4 * d), GELU(), nn.Linear(4 * d, d))
 
     def fn(m):
       if isinstance(m, nn.Linear):
@@ -78,11 +82,17 @@ class GPT(nn.Module):
     l = t.shape[-1]
     assert l <= self.l, f'input sequence length should be <= {self.l}'
     if len(t.shape) == 1:
-      t = t.unsqueeze(0)  # (l,) to (1, l)
+      t = t.unsqueeze(0)  # (l,) to (1, l)   
     xm = (self.emb(t) + self.pos[:l], self.m[:l, :l])
     return self.out(self.layers(xm)[0]).squeeze()
 
-  def loss(self, it, ot):
-    return F.cross_entropy(
-      rearrange(self(it), 'b l v -> (b l) v'), rearrange(ot, 'b l -> (b l)')
+  def loss(self, it, ot, icl=False):
+    ce_loss = F.cross_entropy(
+      rearrange(self(it), 'b l v -> (b l) v'),
+      rearrange(ot, 'b l -> (b l)'),
+      reduction='none'
     )
+    if icl:  # compute in-context learning score
+      with torch.no_grad():
+        icl_score = (ce_loss[-1::self.l] - ce_loss[0::self.l]).mean().item
+    return ce_loss.mean(), icl_score
